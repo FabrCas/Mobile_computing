ae8b6e43e3b29f92bb0b675f1f44c1887f50fee6
Manuel <53404250+manuxx97@users.noreply.github.com>
2019-09-24 13:41:43 +0200

    corretta la funzione di shooting

--- App/levels/scene1.lua
+++ App/levels/scene1.lua
@@ -12,57 +12,110 @@ local _W = display.contentWidth
 local _H = display.contentHeight
 local damage = 15
 local bg = display.newRect( _W/2, _H/2, _W, _H )
-bg.alpha=0.1
-
+bg.alpha=0.01
+local nMattoni = 0 --numero dei mattoni
 local muroSinistra = display.newRect( 0, _H/2, 0 ,_H )
 local muroDestra = display.newRect( _W, _H/2, 0 , _H)
 local muroInBasso = display.newRect( _W/2, _H+15, _W, 0 )
-local muroInAlto = display.newRect( _W/2, 0, _W, 0 )
+local muroInAlto = display.newRect( _W/2, -5, _W, 0 )
 local canShoot = true
  physics.addBody( muroSinistra,"static")
  physics.addBody( muroDestra,"static")
  physics.addBody( muroInBasso,"static")
  physics.addBody( muroInAlto,"static")
  muroInBasso:addEventListener( "collision", function(event)  event.other:removeSelf( ) event.other = nil canShoot = true end )
+ local rect = display.newRect( _W/2, 0, 10, 50 )
+ rect:setFillColor( 0.5,0.5,0.6 )
+ rect.anchorY = 0
 ---------------------------------------------------------------------------------
 -- BEGINNING OF YOUR IMPLEMENTATION
 ---------------------------------------------------------------------------------
 local btn = nil
 local myLevel = {}
+--local p = 10
+function getLevel()
+	return myLevel
+end
+--local rect = display.newRect( 0, _H -100 , 200, 50 )
+--local rect2 = display.newRect( _W, _H -100, 200, 50 )
+--rect2:addEventListener( "tap", function() p= p+10 print(p) end )
+--rect:addEventListener( "tap", function() p= p-10 print(p) end )
 ---------------------------------------------------------------------------------
--- FUNZIONE DI SHOOTING
+-- FUNZIONE DI SHOOTING 
 ---------------------------------------------------------------------------------
-function shoot(event)
-	if canShoot then
-		canShoot = false
+function shoot(event) --i=10 bounce = 0,6 local angle = math.atan2(my - (cerchio.y + event.y/5.5), mx - (cerchio.x +0))
+	  
+	  function getAngle() --FUNZIONE PER IL CALCOLO DELL'ANGOLO DI ROTAZIONE
+    local angolo
+    local latoVerticale = event.y
+    print("il lato verticale e':" .. latoVerticale)
+    print("rect.y=" .. rect.y)
+    local latoObliquo = math.sqrt(math.pow((rect.x - event.x),2)+math.pow((rect.y - event.y),2))
+    print("il lato obliquo e':" .. latoObliquo)
+    angoloRad = (math.acos(latoVerticale/latoObliquo))  --radianti
+    print("angolo radianti = ".. angoloRad)
+    angolo= ((angoloRad*180)/3.14)
+    print("angolo = ".. angolo)
+    if (event.x < _W/2) then
+    return angolo
+    else return -angolo end
+  end --FINE FUNZIONE PER IL CALCOLO DELL'ANGOLO DI ROTAZIONE
+  rect.rotation = getAngle()
+	if canShoot then  
+		--canShoot = false
 	print(event.phase)
-	i=10
-    cerchio = display.newCircle(_W/2,i, i) --creazione palla
+	i=5.5
+    cerchio = display.newCircle(_W/2, 0, i) --creazione palla
     physics.addBody(cerchio,{radius=i,bounce=0.8})
-local mx,my = event.x,event.y
-local angle = math.atan2(my - (cerchio.y + 0), mx - (cerchio.x +0))
-local vx,vy = math.cos(angle)*5, math.sin(angle)*5
- cerchio:applyForce(vx,vy,cerchio.x,cerchio.y)
+deltaX =  event.x-_W/2
+deltaY = event.y
+normDeltaX = deltaX / math.sqrt(math.pow(deltaX,2) + math.pow(deltaY,2))
+normDeltaY = deltaY / math.sqrt(math.pow(deltaX,2) + math.pow(deltaY,2))
+ speed = 700
+ print(normDeltaX.." * "..string.format(speed).."= "..string.format(normDeltaX*speed))
+  print(normDeltaY .. " * " .. string.format(speed).."= " .. string.format(normDeltaY*speed))
+  physics.setGravity(0, 0)
+cerchio:setLinearVelocity( normDeltaX  * speed, normDeltaY  * speed )
     end end
-  bg:addEventListener("tap",shoot)
+ bg:addEventListener("tap",shoot)
+---------------------------------------------------------------------------------
+-- CREAZIONE PALLA ANIMATA
+---------------------------------------------------------------------------------
+function creaSfera()
+	   local cerchioProps = 
+	{
+		name 	= "ball", 
+		x		= 50,
+		y		= 40,
+		assetName	= "balls_preview2_0",
+		xScale = 0.1,
+		yScale = 0.1,
+	}
+	
+	local cerchio = getLevel():createObject("Layer 1", cerchioProps ).view
+	cerchio:setSequence("ball_splash")
+   cerchio:play()
+    end
 ---------------------------------------------------------------------------------
 --FUNZIONE DI RIMOZIONE BLOCCHI
 ---------------------------------------------------------------------------------
 function removeBrick(brick)
 	brick:removeSelf()
 	brick = nil
-end
+	nMattoni = nMattoni - 1
+	if nMattoni == 0 then
+		local txt = display.newText( "Hai vinto!", _W/2, _H/2 , native.systemFont,12 )
+end end
 ---------------------------------------------------------------------------------
 --FUNZIONE QUANDO AVVIENE COLLISIONE TRA BLOCCHI E PALLA
 ---------------------------------------------------------------------------------
 function hit(event)
+	physics.setGravity( 0, 46 )
 	        brick = event.target
             local vx, vy = event.other:getLinearVelocity()
             --calcolo del modulo di vx e vy
             if vx<0 then vx = -vx end
             if vy<0 then vy = -vy end
-
-
           --  if vx == 0 and brick.c==2 then
            -- 	if brick.x > _W/2 then
            -- 		brick.applyForce(-10,5,brick.x,brick.y)
@@ -71,9 +124,8 @@ function hit(event)
           --  	elseif vx==0 and brick.c<2 then
           --  		c= c+1
           --  	end end
-
-			brick.life = brick.life-(damage + vy/10 +vx/10)
-			print("ciao")
+			brick.life = brick.life - (damage + vy/10 +vx/10)
+			--print("ciao")
 
 			if brick.life < 0 then
                removeBrick(brick)
@@ -84,25 +136,43 @@ function hit(event)
 					brick.alpha = 0.25
 				end
 			end
+---------------------------------------------------------------------------------
+--FUNZIONE DI PROVA
+---------------------------------------------------------------------------------
+--[[local pg = display.newImageRect( "images/Owlet_Monster.png",64,64)
+pg.x = 120 pg.y = 0
+--pg.fill.effect = "filter.blurVertical"
+--pg.fill.effect.blurSize = 8
+--pg.fill.effect.sigma = 128
+--blurSize — default = 8 ; min = 2 ; max = 512
+--sigma — default = 128 ; min = 2 ; max = 512
+function moveMonster(event)
+	pg.y = pg.y + 5
+	if (pg.y > _H) then
+		pg.y=-32
+	end
+end
+Runtime:addEventListener( "enterFrame", moveMonster )
+--]]
 
 -- Called when the scene's view does not exist:
 function scene:create( event )
 	local screenGroup = self.view
-	local myLevel = {}
-	myLevel= LD_Loader:new(self.view)
+	--local myLevel = {}
+	myLevel = LD_Loader:new(self.view)
 	myLevel:loadLevel("Level01") -- set your scene/level name here
-  --physics.setDrawMode( "debug" )
-physics.setGravity(0, 30)
+    --physics.setDrawMode( "debug" )
+    creaSfera()
     -- aggiunta listener ai mattoni
 	local obj = {}
 	for i=1,29 do
-	obj[i]= {}
+	obj[i] = {}
 	obj[i] = myLevel:getLayerObject("Layer 1","Brick_"..string.format(i)).view
 	 obj[i]:addEventListener( "preCollision", hit)
 	obj[i].life = 100.00
 	obj[i].c = 0 --conta quante volte in una singola sessione di tiro è stato colpito verticalmente
+	nMattoni = nMattoni + 1
 	end
-
 
 	-- Touch event listener for button
 	function onButtonClick( event )
