4829d54a83d09de2af18f368cb5b79b798fc010e
Manuel <manuel.potrich@gmail.com>
2019-10-03 00:23:15 +0200

    modificata funzione per lancio palla speciale

--- App/lib/ball.lua
+++ App/lib/ball.lua
@@ -2,7 +2,7 @@
 -- There are two types of canonn balls: normal and bomb
 
 local physics = require('physics')
-
+require ("lib.partitaStoria")
 local _M = {}
 
 
@@ -11,13 +11,14 @@ function _M.newBall(sx,sy,potereAttivato)
 	if potereAttivato then
 		local txt = display.newText( "Potere speciale attivo", display.contentWidth/2 +50 ,  30, native.systemFont,12 )
 		 transition.to( txt, { time=1500, alpha=0} )
-	 ball = display.newImageRect("images/RainbowBall.png", 15, 15)
+	 ball = display.newImageRect("images/PallaSpeciale"..partitaS:personaggio()..".png", 15, 15)
 	ball.x  = display.contentWidth/2
 	ball.y = 0
 	physics.addBody(ball, 'static' , {radius=7.5,bounce=0.8})
 	ball.density= 0.73
 	-- While the ball rests near the cannon, it's static
-	ball.isLaunched = false else 
+	ball.isLaunched = false else
+
 	 ball = display.newImageRect("images/blackball.png", 15, 15)
 	ball.x  = display.contentWidth/2
 	ball.y = 0
