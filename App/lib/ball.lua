e6343097de0fbeb37970e70ffa85ef365a54368d
manuxx97 <manuel.potrich@gmail.com>
2019-10-09 17:14:46 +0200

    migliorata la funzione della palla

--- App/lib/ball.lua
+++ App/lib/ball.lua
@@ -7,20 +7,21 @@ local _M = {}
 
 
 function _M.newBall(sx,sy,potereAttivato)
-	
+
 	if potereAttivato then
 	 ball = display.newImageRect("images/PallaSpeciale"..partitaS:personaggio()..".png", 15, 15)
+	 physics.addBody(ball, 'static' , {radius=7.5,bounce=0.8,friction=0.3})
 	else
 
-    ball = display.newImageRect("images/blackball.png", 15, 15)
+    ball = display.newImageRect("images/default ball.png", 15, 15)
+		physics.addBody(ball, 'static' , {radius=7.5,bounce=0.8})
         end
     ball.x  = display.contentWidth/2
     ball.y = 50
-    physics.addBody(ball, 'static' , {radius=7.5,bounce=0.8,friction=0.3})
     ball.density= 0.73
     -- While the ball rests near the cannon, it's static
     ball.isLaunched = false
- 
+
 
 	function ball:launch()
 	   ball.isLaunched = true
@@ -31,7 +32,7 @@ function _M.newBall(sx,sy,potereAttivato)
        normDeltaY = deltaY / math.sqrt(deltaX^2 + deltaY^2)
        speed = 700
        physics.setGravity(0, 0)
-       ball:setLinearVelocity( normDeltaX*speed,normDeltaY*speed )  
+       ball:setLinearVelocity( normDeltaX*speed,normDeltaY*speed )
 	end
 	return ball
 end
